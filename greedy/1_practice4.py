d829528ac8e69e89737187bab0dbd9fe21c32669
이로움 <roum02@jobkorea.co.kr>
2023-03-27 21:23:37 +0900

    (week2) Add greedy 알고리즘 - 실전문제4 [main]

--- greedy/1_practice4.py
+++ greedy/1_practice4.py
@@ -1,3 +1,16 @@
 """
 1이 될 때까지
 """
+
+import sys
+
+n, k = map(int, sys.stdin.readline().split(' '))
+count = 0
+
+while n > 1:
+    if n%k == 0:
+        n = n/k
+    else:
+        n -= 1
+    count += 1
+print(count)
