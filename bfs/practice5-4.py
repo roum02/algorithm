4bf0b3f25d5330c45aeb5e547581e1dfdf4d3244
이로움 <roum02@jobkorea.co.kr>
2023-04-16 22:04:19 +0900

    Add solution of 미로탈출 [week4]

--- bfs/practice5-4.py
+++ bfs/practice5-4.py
@@ -2,9 +2,37 @@
 미로탈출
 """
 import sys
+from collections import deque
 
 read = sys.stdin.readline
 
 n, m = map(int, read().split(" "))
-map_list = [list(map(int, read().rstrip())) for _ in range(n)]
+matrix = [list(map(int, read().rstrip())) for _ in range(n)]
+visited = [[0] * m for _ in range(n)]
 
+# 상하좌우
+dx = [0, 0, -1, 1]
+dy = [1, -1, 0, 0]
+
+
+def bfs(y, x):
+    queue = deque()
+    queue.append((y, x))
+    visited[y][x] = 1
+
+    while queue:
+        y, x = queue.popleft()
+        for i in range(4):
+            ny = y + dy[i]
+            nx = x + dx[i]
+
+            if 0 > ny or ny >= n or 0 > nx or nx >= m:
+                continue
+            if matrix[ny][nx] == 1 and visited[ny][nx] == 0:
+                visited[ny][nx] = visited[y][x] + 1
+                queue.append((ny, nx))
+
+    return visited[n-1][m-1]
+
+
+print(bfs(0, 0))
