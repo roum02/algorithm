813a727d5e582f4fa7d9414bf8896bda90e114f7
이로움 <roum02@jobkorea.co.kr>
2023-04-19 23:48:41 +0900

    Add insert sort example [week6]

---
+++ sort/insertion sort/ex.py
@@ -0,0 +1,15 @@
+"""
+삽입정렬 예시
+=> 데이터를 하나씩 확인하여 적절한 위치에 넣기
+"""
+
+array = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
+
+for i in range(1, len(array)):
+    for j in range(i, 0, -1):
+        if array[j] < array[j-1]:
+            array[j], array[j-1] = array[j-1], array[j]
+        else:
+            break
+
+print(array)
