05541f958a4cacf96f8a2b93fa4735fcfe9d1509
roum <roum02@ewhain.net>
2023-10-19 02:04:21 +0900

    Add input logic [c++]

---
+++ input-output/input.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int a, b;
+    /** 1. the direction of double shift is the opposite.
+     * 2. the 'cin' commend automatically guesses the data type of the variables.
+    */
+    cin >> a >> b;
+
+    cout << a + b << endl;
+}
